a3f4f870a480cc9881f78a73cc28bbc7f800a75b
samiullahmughal77777-afk <samiullahmughal77777@gmail.com>
2026-03-24 13:30:10 +0300

    Update duckhunt.js

--- duckhunt.js
+++ duckhunt.js
@@ -2,14 +2,19 @@
 <html>
 <head>
   <meta charset="UTF-8">
-  <title>Duck Hunt Game</title>
+  <title>Duck Hunt</title>
 </head>
 
-<body style="margin:0; overflow:hidden; background:black;">
-
-<canvas id="game"></canvas>
+<body style="margin:0; background:black;">
 
 <script src="duckhunt.js"></script>
+
+<script>
+  // force start
+  if (typeof startGame === "function") {
+    startGame();
+  }
+</script>
 
 </body>
 </html>
